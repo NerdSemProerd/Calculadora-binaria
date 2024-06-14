ec2b2c78a8d287face39be8edffc59c92f8a694f
emanuell.rossetto <emanuellrossetto@hotmail.com>
2024-06-14 12:25:20 -0300

    Filtro de campos concluido

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -10,12 +10,49 @@ function filtro_conversor(event){
     console.log(conv_de)
     switch (true) {
         case conv_de === 'dec':
-            let input = event.target.value;
-            let filtro = input.replace(/[^0-9]/g, '')
-            event.target.value = filtro;
+            filtro_dec(event)
             break;
-            
+        case conv_de === 'bin':
+            filtro_bin(event)
+            break;
+        case conv_de === 'oct':
+            filtro_oct(event)
+            break;
+        case conv_de === 'hex':
+            filtro_hex(event)
+            break;
+        case conv_de === 'com':
+            filtro_com(event)
+            break;
+        
         default:
             break;
     }
 }
+function filtro_dec(event){
+    let input = event.target.value;
+    let filtro = input.replace(/[^0-9]/g, '')
+    event.target.value = filtro;
+}
+function filtro_bin(event){
+    let input = event.target.value;
+    let filtro = input.replace(/[^01]/g, '')
+    event.target.value = filtro;
+}
+function filtro_oct(event){
+    let input = event.target.value;
+    let filtro = input.replace(/[^0-8]/g, '')
+    event.target.value = filtro;
+}
+function filtro_hex(event){
+    let input = event.target.value;
+    let filtro = input.replace(/[^0-9A-Fa-f]/g, '')
+    event.target.value = filtro;
+}
+function filtro_com(event){
+    let input = event.target.value;
+    let filtro = input.replace(/[^0-9A-Fa-f]/g, '')
+    event.target.value = filtro;
+}
+
+
