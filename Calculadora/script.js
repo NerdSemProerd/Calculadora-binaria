26478af09f86e18fb62ac93b681433823d6a13fd
NerdSemProerd <emanuellrossetto@hotmail.com>
2024-06-14 11:59:36 -0300

    HTML terminado, adicionado Filtro de caracteres

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,63 +1,21 @@
-function converter() {
-    let tipoDe = document.getElementById('tipoDe').value;
-    let tipoPara = document.getElementById('tipoPara').value;
-
-    let valorEntrada = document.getElementById('valorEntrada').value.trim();
-
-    if (valorEntrada === '') {
-        alert("Por favor, insira um valor para converter.");
-        return;
+//Filtro que evita que outros numeros sejam escritos
+//Usar essa função na TAG em HTML: oninput="filtro_calc(event)"
+function filtro_calc(event) {  
+    let input = event.target.value;  // Obter o valor atual do campo de entrada
+    let filtro = input.replace(/[^01]/g, ''); //Tudo que não for 0 e 1 é substituido por "Nada"
+    event.target.value = filtro;  // Atualizar o valor do campo de entrada com o valor filtrado
+}
+function filtro_conversor(event){
+    var conv_de = document.getElementById('De').value;
+    console.log(conv_de)
+    switch (true) {
+        case conv_de === 'dec':
+            let input = event.target.value;
+            let filtro = input.replace(/[^0-9]/g, '')
+            event.target.value = filtro;
+            break;
+            
+        default:
+            break;
     }
-
-    let valorNumerico = parseFloat(valorEntrada);
-    if (isNaN(valorNumerico)) {
-        alert("Por favor, insira um valor numérico válido para converter.");
-        return;
-    }
-
-    let resultado = '';
-
-    if (tipoDe === 'decimal') {
-        resultado = converterDeDecimal(valorNumerico, tipoPara);
-    } else if (tipoDe === 'binario') {
-        resultado = converterDeBinario(valorEntrada, tipoPara);
-    } else if (tipoDe === 'octal') {
-        resultado = converterDeOctal(valorEntrada, tipoPara);
-    } else if (tipoDe === 'hexadecimal') {
-        resultado = converterDeHexadecimal(valorEntrada, tipoPara);
-    } else if (tipoDe === 'complemento2') {
-        resultado = converterDeComplemento2(valorEntrada, tipoPara);
-    } else {
-        alert("Tipo de entrada inválido.");
-        return;
-    }
-
-    document.getElementById('resultado').value = resultado;
 }
-
-function converterDeDecimal(valor, tipoPara) {
-    return 'Converter de Decimal para ' + tipoPara;
-}
-
-function converterDeBinario(valor, tipoPara) {
-    return 'Converter de Binário para ' + tipoPara;
-}
-
-function converterDeOctal(valor, tipoPara) {
-    return 'Converter de Octal para ' + tipoPara;
-}
-
-function converterDeHexadecimal(valor, tipoPara) {
-    return 'Converter de Hexadecimal para ' + tipoPara;
-}
-
-function converterDeComplemento2(valor, tipoPara) {
-    return 'Converter de Complemento de 2 para ' + tipoPara;
-}
-
-function limpar() {
-    document.getElementById('valorEntrada').value = '';
-    document.getElementById('result').value = '';
-}
-
-
