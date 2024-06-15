07b2f7845ca2233fdd86318ce2cf9cc58950859e
NerdSemProerd <emanuellrossetto@hotmail.com>
2024-06-15 11:59:15 -0300

    Trabalho atualizado

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -59,4 +59,46 @@ function filtro_com(event){
     event.target.value = filtro;
 }
 
+function conversao_calc() {
+    let operacao = document.getElementById('operacao').value;
+    let val_bin1 = document.getElementById('calc_ent01').value;
+    let val_bin2 = document.getElementById('calc_ent02').value;
+    let valor1_bin = parseInt(val_bin1, 2)
+    let valor2_bin = parseInt(val_bin2, 2)
+    console.log("Valor 1 ", valor1_bin, " Valor 2 ", valor2_bin)
+
+    switch (true) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+
+}  
+
+function converter(event){
+    var conv_para = document.getElementById('Para').value;
+    
+    switch (conv_para) {
+        case 'dec':
+            event.target.value = convertToDecimal(input, conv_de);
+            break;
+        case 'bin':
+            event.target.value = convertToBinary(input, conv_de);
+            break;
+        case 'oct':
+            event.target.value = convertToOctal(input, conv_de);
+            break;
+        case 'hex':
+            event.target.value = convertToHexadecimal(input, conv_de);
+            break;
+        case 'com':
+            event.target.value = convertToComplementoDeDois(input, conv_de);
+            break;
+        default:
+            break;
+    }
+}
 
